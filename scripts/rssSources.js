a693edc8f425a8c8ca8b711abca2ca9d2d62f15f
Ankitbuilder <ankittewari0@gmail.com>
2026-02-25 15:29:03 +0530

    RSS FEED NEW

--- scripts/rssSources.js
+++ scripts/rssSources.js
@@ -1,10 +1,27 @@
-// scripts/rssSources.js
-
 export const RSS_SOURCES = [
-  "https://indianexpress.com/section/india/feed/",
-  "https://indianexpress.com/section/explained/feed/",
+
+  // ===== PIB (Government – Highest Priority) =====
+  "https://pib.gov.in/RssMain.aspx?ModId=6&Lang=1&Regid=3",
+  "https://pib.gov.in/RssMain.aspx?ModId=18&Lang=1&Regid=3",
+
+  // ===== THE HINDU =====
   "https://www.thehindu.com/news/national/feeder/default.rss",
   "https://www.thehindu.com/news/international/feeder/default.rss",
-  "https://prsindia.org/rss.xml",
-  "https://www.pib.gov.in/rss.aspx"
+  "https://www.thehindu.com/opinion/editorial/feeder/default.rss",
+  "https://www.thehindu.com/opinion/op-ed/feeder/default.rss",
+  "https://www.thehindu.com/business/feeder/default.rss",
+  "https://www.thehindu.com/sci-tech/science/feeder/default.rss",
+  "https://www.thehindu.com/sci-tech/technology/feeder/default.rss",
+  "https://www.thehindu.com/news/states/feeder/default.rss",
+  "https://www.thehindu.com/environment/feeder/default.rss",
+
+  // ===== INDIAN EXPRESS =====
+  "https://indianexpress.com/section/india/feed/",
+  "https://indianexpress.com/section/explained/feed/",
+  "https://indianexpress.com/section/political-pulse/feed/",
+  "https://indianexpress.com/section/business/feed/",
+  "https://indianexpress.com/section/world/feed/",
+  "https://indianexpress.com/section/opinion/feed/",
+  "https://indianexpress.com/section/education/feed/",
+  "https://indianexpress.com/section/science/feed/"
 ];
