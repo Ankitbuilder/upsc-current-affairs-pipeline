a06ee2dd3ce4133eb7cdf8a469dc42010ee67b26
Ankitbuilder <ankittewari0@gmail.com>
2026-02-25 20:42:22 +0530

    Remove leading dot from state file path

--- scripts/stateManager.js
+++ scripts/stateManager.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const stateFilePath = path.join(__dirname, ".processedLinks.json");
+const stateFilePath = path.join(__dirname, "processedLinks.json");
 
 function ensureStateFile() {
   if (!fs.existsSync(stateFilePath)) {
