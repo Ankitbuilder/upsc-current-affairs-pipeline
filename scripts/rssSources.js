22790a04a304446c91e84a64ace59381ea24a330
Ankitbuilder <ankittewari0@gmail.com>
2026-02-25 14:21:08 +0530

    Add RSS sources to rssSources.js

---
+++ scripts/rssSources.js
@@ -0,0 +1,8 @@
+export const RSS_SOURCES = [
+  "https://www.pib.gov.in/rss.aspx"
+  "https://indianexpress.com/section/india/feed/",
+  "https://indianexpress.com/section/explained/feed/",
+  "https://www.thehindu.com/news/national/feeder/default.rss",
+  "https://www.thehindu.com/news/international/feeder/default.rss",
+  "https://prsindia.org/rss.xml",
+  ];
