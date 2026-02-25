a96841958ba418f6d45ce2169363f5d159ad7eee
Ankitbuilder <ankittewari0@gmail.com>
2026-02-25 14:29:16 +0530

    RSS_SOURCES array

--- scripts/rssSources.js
+++ scripts/rssSources.js
@@ -1,8 +1,10 @@
+// scripts/rssSources.js
+
 export const RSS_SOURCES = [
-  "https://www.pib.gov.in/rss.aspx"
   "https://indianexpress.com/section/india/feed/",
   "https://indianexpress.com/section/explained/feed/",
   "https://www.thehindu.com/news/national/feeder/default.rss",
   "https://www.thehindu.com/news/international/feeder/default.rss",
   "https://prsindia.org/rss.xml",
-  ];
+  "https://www.pib.gov.in/rss.aspx"
+];
